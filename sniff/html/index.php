03477579f5ed7a5fd56861c1c87e2107f130b80f
fatih <fatih0537@hotmail.com>
2011-07-23 04:56:49 +0300

    [init] display-errors set to 0

--- sniff/html/index.php
+++ sniff/html/index.php
@@ -1,7 +1,9 @@
 <?php
 
 error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors',1);
+//Dispalying errors is dengerous, user can see file paths when an error occour
+ini_set('display_errors',0);//changed to "0"
+ 
 define('DSN', 'mysql://root@127.0.0.1/sniff');
 
 require __DIR__ . '/autoload.php';
@@ -13,5 +15,4 @@ $factory  = new sniffFactory();
 
 $sniff = new sniffFrontController($request, $response, $factory);
 $view   = $sniff->execute(isset($_GET["controller"]) ? $_GET['controller'] : 'home');
-
 echo $view->render($request, $response);
