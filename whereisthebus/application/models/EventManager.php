0175bdd94bf5dcf6be089d1e4300883df7f42d8b
fatih <fatih0537@hotmail.com>
2011-07-26 02:09:53 +0300

    [models][EventManager] callback function added

--- whereisthebus/application/models/EventManager.php
+++ whereisthebus/application/models/EventManager.php
@@ -4,7 +4,20 @@
  * @author fth
  */
 class Model_EventManager {
+    private $_eventCallbacks = array();
+    private $_subscribers = array();
     
+    function registerEventCallback($callback) {
+        $this->_callbacks[] = $callback;
+    }
+    
+    public function publish($event){
+            $callbacks = $this->subscribers[$event->getHead->getName];
+            
+            foreach($this->callbacks as $this->callback){
+            $this->callback(event);
+            }
+    }  
 }
 
 ?>
